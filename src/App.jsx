34e9d1038759e3cd8ada9db27e0f975a88c960b8
Your Name <you@example.com>
2026-01-01 22:27:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Nav from './Components/Nav/Nav'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Projects from './Components/Projects/Projects'
+import Contact from './Components/Contact/Contact'
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
